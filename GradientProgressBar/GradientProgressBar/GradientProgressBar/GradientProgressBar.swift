f060f34bd599b9f995d85d1cc82d53d44953fc96
Zhenjiang Liu <zliu@kpd-i.com>
2018-08-21 23:21:43 -0400

    init commit

---
+++ GradientProgressBar/GradientProgressBar/GradientProgressBar/GradientProgressBar.swift
@@ -0,0 +1,21 @@
+//
+//  GradientProgressBar.swift
+//  GradientProgressBar
+//
+//  Created by ZhongZhongzhong on 2018-08-21.
+//  Copyright © 2018 zhenjiang. All rights reserved.
+//
+
+import UIKit
+
+class GradientProgressBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GradientProgressBar/GradientProgressBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradientProgressBar
+//
+//  Created by ZhongZhongzhong on 2018-08-21.
+//  Copyright © 2018 zhenjiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
